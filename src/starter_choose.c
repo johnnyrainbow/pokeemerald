d78f2de25ad9033450133649014699dcab2f3635
thunderdoof <a.machungar@gmail.com>
2020-02-07 16:57:30 +1300

    starters

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -126,9 +126,9 @@ static const u8 sStarterLabelCoords[][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-   SPECIES_FLAREON,
-    SPECIES_VAPOREON,
-    SPECIES_JOLTEON,
+   SPECIES_TORCHIC,
+    SPECIES_CHARMANDER,
+    SPECIES_CYNDAQUIL
 };
 
 static const struct BgTemplate gUnknown_085B1E00[3] =
